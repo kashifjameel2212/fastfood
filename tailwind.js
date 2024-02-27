63ad242488e99aa5b01ee30c7f37d171a1f8ad99
kashif <03214752508a@gmail.com>
2024-02-27 18:42:25 +0500

    first commit

---
+++ tailwind.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        container: {
+            center:true,
+            padding: {
+              DEFAULT: '1rem',
+              sm: '2rem',
+              lg: '4rem',
+              xl: '5rem',
+              '2xl': '6rem',
+            },
+            fontFamily: {
+              'body': ['"Open Sans"',],
+            }
+          },
+      }
+    }
+  }
